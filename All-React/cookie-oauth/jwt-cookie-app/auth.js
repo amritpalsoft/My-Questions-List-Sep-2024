b1ccb09ff346c8c8cde0c29098858f139dacdac0
himanshub1995 <94344475+himanshub1995@users.noreply.github.com>
2024-09-23 16:38:30 +0530

    my-all-questions2

--- All-React/cookie-oauth/jwt-cookie-app/auth.js
+++ All-React/cookie-oauth/jwt-cookie-app/auth.js
@@ -4,8 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID || "379687908249-qdp1n1d6l1vilfpmpctln350ko4pgagm.apps.googleusercontent.com",
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET || "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",
+    // clientID: process.env.GOOGLE_CLIENT_ID || "379687908249-qdp1n1d6l1vilfpmpctln350ko4pgagm.apps.googleusercontent.com",
+    // clientSecret: process.env.GOOGLE_CLIENT_SECRET || "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",
     callbackURL: '/auth/google/callback'
 },
     (token, tokenSecret, profile, done) => {
